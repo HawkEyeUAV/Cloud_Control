8a1e0f7ea38c39a41019c58725edca9a20760048
HawkEyeUAV <601689382@qq.com>
2017-06-10 12:30:49 +0800

    add MySQL code

--- mosquitto_cloud.py
+++ mosquitto_cloud.py
@@ -1,7 +1,11 @@
-#!
-# -*- coding: utf-8 -*-    
+#!/usr/bin/env python
+#encoding:utf-8    
 import paho.mqtt.client as mqtt   
 import json  
+import time
+import re
+import MySQLdb as mdb
+import sys
 �  
 def on_connect(client, userdata, flags, rc):  
     print("Connected with result code " + str(rc))  
@@ -10,7 +14,12 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):  
     print(msg.topic+" "+str(msg.payload))  
     # 获得负载中的pin 和 value  
-  
+    con = mdb.connect('127.0.0.1','root','19950419li','tencentcloud')
+        cur = con.cursor()
+        cur.execute('INSERT INTO home(id,updatetime,tem,hum,distance) VALUES(null,null,%s,%s,%s)',c)
+        con.commit()
+        cur.close()
+        con.close() 
 if __name__ == '__main__':  
     client = mqtt.Client()  
     client.on_connect = on_connect  
